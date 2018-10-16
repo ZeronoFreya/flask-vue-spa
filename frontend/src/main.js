d96754a66de27bee456b074d042d6cbf7fff19f5
zeronofreya <zeronofreya@gmail.com>
2018-10-16 17:45:01 +0800

    new

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,28 +1,10 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import 'normalize.css'
-import './assets/css/base.css'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import fontawesome from '@fortawesome/fontawesome'
-import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import solid from '@fortawesome/fontawesome-free-solid'
-import regular from '@fortawesome/fontawesome-free-regular'
-import brands from '@fortawesome/fontawesome-free-brands'
+Vue.config.productionTip = false;
 
-fontawesome.library.add(solid)
-fontawesome.library.add(regular)
-fontawesome.library.add(brands)
-
-Vue.component('fa', FontAwesomeIcon)
-
-Vue.config.productionTip = false
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  render: h => h(App),
-  data: {
-    eventHub: new Vue()
-  }
-})
+  render: h => h(App)
+}).$mount("#app");
--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def random_number():
 
 
 
-@app.route('/', defaults={'path': ''})
+@app.route('/', defaults={'path': ''}) 
 @app.route('/<path:path>')
 def catch_all(path):
     if app.debug:
@@ -30,5 +30,5 @@ def catch_all(path):
     return render_template("index.html")
 
 if __name__ == '__main__':
-    # app.debug = True
+    app.debug = True
     app.run()
